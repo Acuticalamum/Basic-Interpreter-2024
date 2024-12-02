5880b6993fec08c773d23e7f08b55d7c1d0bf32f
Acuticalamum <acuticalamum007@sjtu.edu.cn>
2024-12-02 23:57:50 +0800

    Update Basic.cpp

--- Basic/Basic.cpp
+++ Basic/Basic.cpp
@@ -1,166 +1,121 @@
 /*
- * File: program.h
+ * File: Basic.cpp
  * ---------------
- * This interface exports a Program class for storing a BASIC
- * program.
+ * This file is the starter project for the BASIC interpreter.
  */
 
-#ifndef _program_h
-#define _program_h
-
+#include <cctype>
+#include <iostream>
 #include <string>
-#include <vector>
-#include <set>
-#include <unordered_map>
-#include "statement.hpp"
+#include "exp.hpp"
+#include "parser.hpp"
+#include "program.hpp"
+#include "Utils/error.hpp"
+#include "Utils/tokenScanner.hpp"
+#include "Utils/strlib.hpp"
 
 
-class Statement;
+/* Function prototypes */
 
-/*
- * This class stores the lines in a BASIC program.  Each line
- * in the program is stored in order according to its line number.
- * Moreover, each line in the program is associated with two
- * components:
- *
- * 1. The source line, which is the complete line (including the
- *    line number) that was entered by the user.
- *
- * 2. The parsed representation of that statement, which is a
- *    pointer to a Statement.
- */
+void processLine(std::string line, Program &program, EvalState &state);
 
-class Program {
+/* Main program */
 
-public:
-
-/*
- * Constructor: Program
- * Usage: Program program;
- * -----------------------
- * Constructs an empty BASIC program.
- */
-
-    Program();
+int main() {
+    EvalState state;
+    Program program;
+    //cout << "Stub implementation of BASIC" << endl;
+    while (true) {
+        try {
+            std::string input;
+            getline(std::cin, input);
+            if (input.empty())
+                continue;
+            processLine(input, program, state);
+        } catch (ErrorException &ex) {
+            std::cout << ex.getMessage() << std::endl;
+        }
+    }
+    return 0;
+}
 
 /*
- * Destructor: ~Program
- * Usage: usually implicit
- * -----------------------
- * Frees any heap storage associated with the program.
+ * Function: processLine
+ * Usage: processLine(line, program, state);
+ * -----------------------------------------
+ * Processes a single line entered by the user.  In this version of
+ * implementation, the program reads a line, parses it as an expression,
+ * and then prints the result.  In your implementation, you will
+ * need to replace this method with one that can respond correctly
+ * when the user enters a program line (which begins with a number)
+ * or one of the BASIC commands, such as LIST or RUN.
  */
 
-    ~Program();
+void processLine(std::string line, Program &program, EvalState &state) {
+    TokenScanner scanner;
+    scanner.ignoreWhitespace();
+    scanner.scanNumbers();
+    scanner.setInput(line);
 
-/*
- * Method: clear
- * Usage: program.clear();
- * -----------------------
- * Removes all lines from the program.
- */
+    std::string token;
+    TokenType tokentype = STRING;
+    if(scanner.hasMoreTokens()) {
+      token = scanner.nextToken();
+      tokentype = scanner.getTokenType(token);
+    }
+    else return;
 
-    void clear();
+    /*dealing with special orders*/
+    if(token == "RUN") {
+      try {
+        program.run_the_program(state);
+      } catch (ErrorException &ex) {
+        std::cout << ex.getMessage() << std::endl;
+      }
+      return;
+    }
+    if(token == "LIST") {
+      int key = program.getFirstLineNumber();
+      while(key != -1) {
+        std::cout << program.getSourceLine(key) << std::endl;
+        key = program.getNextLineNumber(key);
+      }
+      return;
+    }
+    if(token == "CLEAR") {
+      program.clear();
+      state.Clear();
+      return;
+    }
+    if(token == "QUIT") {
+      exit(0);
+    }
+    if(token == "HELP") {
+      std::cout << "Damn the BASIC Interpreter!!!" << std::endl;
+      return;
+    }
+    
+    /*dealing with number-prefixed lines*/
+    if(tokentype == NUMBER) {
+      int lineNumber = stringToInteger(token);
+      try {
+        program.addSourceLine(lineNumber, line);
+      } catch(ErrorException &ex) {
+        std::cout << ex.getMessage() << std::endl;
+      }
+    }
 
-/*
- * Method: addSourceLine
- * Usage: program.addSourceLine(lineNumber, line);
- * -----------------------------------------------
- * Adds a source line to the program with the specified line number.
- * If that line already exists, the text of the line replaces
- * the text of any existing line and the parsed representation
- * (if any) is deleted.  If the line is new, it is added to the
- * program in the correct sequence.
- */
-
-    void addSourceLine(int lineNumber, const std::string& line);
-
-/*
- * Method: removeSourceLine
- * Usage: program.removeSourceLine(lineNumber);
- * --------------------------------------------
- * Removes the line with the specified number from the program,
- * freeing the memory associated with any parsed representation.
- * If no such line exists, this method simply returns without
- * performing any action.
- */
-
-    void removeSourceLine(int lineNumber);
-
-/*
- * Method: getSourceLine
- * Usage: string line = program.getSourceLine(lineNumber);
- * -------------------------------------------------------
- * Returns the program line with the specified line number.
- * If no such line exists, this method returns the empty string.
- */
-
-    std::string getSourceLine(int lineNumber);
-
-/*
- * Method: setParsedStatement
- * Usage: program.setParsedStatement(lineNumber, stmt);
- * ----------------------------------------------------
- * Adds the parsed representation of the statement to the statement
- * at the specified line number.  If no such line exists, this
- * method raises an error.  If a previous parsed representation
- * exists, the memory for that statement is reclaimed.
- */
-
-    void setParsedStatement(int lineNumber, Statement *stmt);
-
-/*
- * Method: getParsedStatement
- * Usage: Statement *stmt = program.getParsedStatement(lineNumber);
- * ----------------------------------------------------------------
- * Retrieves the parsed representation of the statement at the
- * specified line number.  If no value has been set, this method
- * returns NULL.
- */
-
-    Statement *getParsedStatement(int lineNumber);
-
-/*
- * Method: getFirstLineNumber
- * Usage: int lineNumber = program.getFirstLineNumber();
- * -----------------------------------------------------
- * Returns the line number of the first line in the program.
- * If the program has no lines, this method returns -1.
- */
-
-    int getFirstLineNumber();
-
-/*
- * Method: getNextLineNumber
- * Usage: int nextLine = program.getNextLineNumber(lineNumber);
- * ------------------------------------------------------------
- * Returns the line number of the first line in the program whose
- * number is larger than the specified one, which must already exist
- * in the program.  If no more lines remain, this method returns -1.
- */
-
-    int getNextLineNumber(int lineNumber);
-
-    //more func to add
+    /*dealing with other lines*/
+    if(tokentype == STRING) {
+      if(token == "LET" || token == "PRINT" || token == "INPUT") {
+        Statement* statement = transfer(line);
+        statement -> execute(state, program);
+      }
+      else {
+        std::cout << "SYNTAX ERROR" << std::endl;
+      }
+      //SYNTAX ERROR
+    }
     //todo
+}
 
-    void endProgram();
-
-    void switchCurrentLine(int lineNumber);
-
-    void nextCurrentLine();
-
-    void run_the_program(EvalState &state);
-
-private:
-
-    // Fill this in with whatever types and instance variables you need
-    //todo
-
-    std::unordered_map <int, std::string> map_string;
-    std::unordered_map <int, Statement*> map_statement;
-    std::set<int> lines;
-    int CurrentLine;
-    bool Ended;
-};
-
-#endif
