3d3f150e890ae51400a3fa2d599c2a24afa3c1a6
Acuticalamum <acuticalamum007@sjtu.edu.cn>
2024-12-02 23:59:45 +0800

    Update statement.cpp

--- Basic/statement.cpp
+++ Basic/statement.cpp
@@ -19,3 +19,60 @@ Statement::Statement() = default;
 Statement::~Statement() = default;
 
 //todo
+
+Statement* transfer(const std::string &str) {
+  TokenScanner scanner;
+  scanner.ignoreWhitespace();
+  scanner.scanNumbers();
+  scanner.setInput(str);
+  std::string indicator, token;
+  Statement *statement = nullptr;
+  Expression *expr1 = nullptr, *expr2 = nullptr;
+  indicator = scanner.nextToken();
+  if(indicator == "REM") {
+    return new RemStatement;
+  }
+  else if(indicator == "LET") {
+    try {
+      statement = new LetStatement(scanner);
+      return statement;
+    } catch (ErrorException &ex) {
+      delete statement;
+      throw ex;
+    }
+  }
+  else if(indicator == "PRINT") {
+    try {
+      expr1 = parseExp(scanner);
+      statement = new PrintStatement(expr1);
+      return statement;
+    } catch (ErrorException &ex) {
+      delete statement;
+      throw ex;
+    }
+  }
+  else if(indicator == "INPUT") {
+    try {
+      expr1 = parseExp(scanner);
+      if(expr1 -> getType() == IDENTIFIER) {
+        token = scanner.nextToken();
+        statement = new InputStatement(token);
+      } else {
+        error("SYNTAX ERROR");
+      }
+    } catch(ErrorException &ex) {
+      delete statement;
+      throw ex;
+    }
+  }
+  else if(indicator == "END") {
+    return new EndStatement;
+  }
+  else if(indicator == "IF") {
+    
+  }
+  else if(indicator == "GOTO") {
+    
+  }
+  else error("SYNTAX MESSAGE");
+}
